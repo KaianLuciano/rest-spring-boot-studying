0a0d05f0ea437580dac0823e17fe0089e0194faa
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-18 17:22:44 -0300

    Init Project

---
+++ src/test/java/br/com/rest/spring/RestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rest.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
