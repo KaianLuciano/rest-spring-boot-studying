fbd2a078d48812f8e14987ee0eb171583ebdf084
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-19 20:13:09 -0300

    Create entity Person

---
+++ src/main/java/br/com/rest/spring/model/Person.java
@@ -0,0 +1,15 @@
+package br.com.rest.spring.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public class Person {
+
+    private Long personId;
+    String firstName;
+    private String lastName;
+    private String address;
+    private String gender;
+}
