ca68693a1698fd7ab0f42e1d063e2c136c784ec6
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-19 20:14:34 -0300

    Setup entity Person

--- src/main/java/br/com/rest/spring/model/Person.java
+++ src/main/java/br/com/rest/spring/model/Person.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Objects;
+
 public class Person {
 
     private Long personId;
@@ -12,4 +14,66 @@ public class Person {
     private String lastName;
     private String address;
     private String gender;
+
+    public Person() {}
+
+    public Person(Long personId, String firstName, String lastName, String address, String gender) {
+        this.personId = personId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.gender = gender;
+    }
+
+    public Long getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(Long personId) {
+        this.personId = personId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person person)) return false;
+        return Objects.equals(getPersonId(), person.getPersonId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getPersonId());
+    }
 }
