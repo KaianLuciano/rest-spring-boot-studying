c689242137a95b72f275c7903faea93205285df1
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-19 20:24:45 -0300

    Create PersonService

---
+++ src/main/java/br/com/rest/spring/service/PersonService.java
@@ -0,0 +1,23 @@
+package br.com.rest.spring.service;
+
+import br.com.rest.spring.PersonRepository;
+import br.com.rest.spring.model.Person;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Slf4j
+@Service
+public class PersonService {
+    private final PersonRepository personRepository;
+
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public Person findById(Long id) {
+        log.info("Find person");
+        return personRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Person not found"));
+    }
+}
