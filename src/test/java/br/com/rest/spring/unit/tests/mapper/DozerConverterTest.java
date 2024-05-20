88aa70b8c480e2d09b6a42bdba0ad55bea1802e6
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-20 20:37:13 -0300

    Create parseEntityToVOTest in DozerConverterTest

--- src/test/java/br/com/rest/spring/unit/tests/mapper/DozerConverterTest.java
+++ src/test/java/br/com/rest/spring/unit/tests/mapper/DozerConverterTest.java
@@ -1,4 +1,33 @@
 package br.com.rest.spring.unit.tests.mapper;
 
+import br.com.rest.spring.data.vo.v1.PersonVO;
+import br.com.rest.spring.mapper.DozerMapper;
+import br.com.rest.spring.model.Person;
+import br.com.rest.spring.unit.tests.mapper.mocks.MockPerson;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DozerConverterTest {
+
+    MockPerson inputObject;
+
+    @BeforeEach
+    public void setUp() {
+        inputObject = new MockPerson();
+    }
+
+    @Test
+    public void parseEntityToVOTest() {
+        PersonVO output = DozerMapper.parseObject(inputObject.mockEntity(), PersonVO.class);
+        assertEquals(Long.valueOf(0L), output.getPersonId());
+        assertEquals("First Name Test0", output.getFirstName());
+        assertEquals("Last Name Test0", output.getLastName());
+        assertEquals("Addres Test0", output.getAddress());
+        assertEquals("Male", output.getGender());
+    }
+
 }
