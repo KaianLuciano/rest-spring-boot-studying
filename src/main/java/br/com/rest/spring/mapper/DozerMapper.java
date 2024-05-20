6e999b5829e6bbc3ca9fb3641f7ed6c1aa6a63cc
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-20 20:29:43 -0300

    Create methods parseObject and parseListObjects

--- src/main/java/br/com/rest/spring/mapper/DozerMapper.java
+++ src/main/java/br/com/rest/spring/mapper/DozerMapper.java
@@ -10,4 +10,15 @@ public class DozerMapper {
 
     private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
 
+    public static <O, D> D parseObject(O origin, Class<D> destination) {
+        return mapper.map(origin, destination);
+    }
+
+    public static <O, D> List<D> parseListObjects(List<O> origin, Class<D> destination) {
+        List<D> destinationObjects = new ArrayList<D>();
+        for (O o : origin) {
+            destinationObjects.add(mapper.map(o, destination));
+        }
+        return destinationObjects;
+    }
 }
