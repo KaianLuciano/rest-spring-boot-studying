65ba977531f11f8a9df3528e09f549c950b1aef1
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-20 20:37:28 -0300

    Create parseEntityListToVOListTest in DozerConverterTest

--- src/test/java/br/com/rest/spring/unit/tests/mapper/DozerConverterTest.java
+++ src/test/java/br/com/rest/spring/unit/tests/mapper/DozerConverterTest.java
@@ -30,4 +30,32 @@ public class DozerConverterTest {
         assertEquals("Male", output.getGender());
     }
 
+    @Test
+    public void parseEntityListToVOListTest() {
+        List<PersonVO> outputList = DozerMapper.parseListObjects(inputObject.mockEntityList(), PersonVO.class);
+        PersonVO outputZero = outputList.get(0);
+
+        assertEquals(Long.valueOf(0L), outputZero.getPersonId());
+        assertEquals("First Name Test0", outputZero.getFirstName());
+        assertEquals("Last Name Test0", outputZero.getLastName());
+        assertEquals("Addres Test0", outputZero.getAddress());
+        assertEquals("Male", outputZero.getGender());
+
+        PersonVO outputSeven = outputList.get(7);
+
+        assertEquals(Long.valueOf(7L), outputSeven.getPersonId());
+        assertEquals("First Name Test7", outputSeven.getFirstName());
+        assertEquals("Last Name Test7", outputSeven.getLastName());
+        assertEquals("Addres Test7", outputSeven.getAddress());
+        assertEquals("Female", outputSeven.getGender());
+
+        PersonVO outputTwelve = outputList.get(12);
+
+        assertEquals(Long.valueOf(12L), outputTwelve.getPersonId());
+        assertEquals("First Name Test12", outputTwelve.getFirstName());
+        assertEquals("Last Name Test12", outputTwelve.getLastName());
+        assertEquals("Addres Test12", outputTwelve.getAddress());
+        assertEquals("Male", outputTwelve.getGender());
+    }
+
 }
