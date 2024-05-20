be6ffe5c5b736ab3c1b056d66484ac4e19c92d57
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-20 20:29:19 -0300

    Create class DozerMapper

---
+++ src/main/java/br/com/rest/spring/mapper/DozerMapper.java
@@ -0,0 +1,13 @@
+package br.com.rest.spring.mapper;
+
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DozerMapper {
+
+    private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+
+}
