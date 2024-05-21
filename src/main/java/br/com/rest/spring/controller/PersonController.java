92424642b0e0da9245796b86df1fef650220acbf
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-21 17:17:09 -0300

    Added Flyway in project

--- src/main/java/br/com/rest/spring/controller/PersonController.java
+++ src/main/java/br/com/rest/spring/controller/PersonController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person/v1")
 public class PersonController {
     private final PersonService personService;
 
