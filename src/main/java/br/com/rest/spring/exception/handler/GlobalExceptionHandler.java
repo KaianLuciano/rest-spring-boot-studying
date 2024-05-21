e454e579f35f2a47ec89bd1f55a99eddbc5f63af
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-21 08:31:22 -0300

    Create GlobalExceptionHandler

---
+++ src/main/java/br/com/rest/spring/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package br.com.rest.spring.exception.handler;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}
