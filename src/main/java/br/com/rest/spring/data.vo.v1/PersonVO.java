2cc10d02db0332b364deaffa6ebbc734a1c45704
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-21 08:34:21 -0300

    Update PersonVO added Getter and Setter of Lombook

--- src/main/java/br/com/rest/spring/data.vo.v1/PersonVO.java
+++ src/main/java/br/com/rest/spring/data.vo.v1/PersonVO.java
@@ -1,11 +1,16 @@
 package br.com.rest.spring.data.vo.v1;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Objects;
 
+@Getter
+@Setter
 public class PersonVO {
 
     private Long personId;
-    String firstName;
+    private String firstName;
     private String lastName;
     private String address;
     private String gender;
@@ -17,46 +22,6 @@ public class PersonVO {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
-        this.gender = gender;
-    }
-
-    public Long getPersonId() {
-        return personId;
-    }
-
-    public void setPersonId(Long personId) {
-        this.personId = personId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
         this.gender = gender;
     }
 
