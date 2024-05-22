74b0dc51c781ecc2ae494e75b974b37fc1e5e01b
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-22 17:14:14 -0300

    Added HATEOAS in PersonService

--- src/main/java/br/com/rest/spring/service/PersonService.java
+++ src/main/java/br/com/rest/spring/service/PersonService.java
@@ -1,5 +1,6 @@
 package br.com.rest.spring.service;
 
+import br.com.rest.spring.controller.PersonController;
 import br.com.rest.spring.data.vo.v1.PersonVO;
 import br.com.rest.spring.data.vo.v2.PersonVOV2;
 import br.com.rest.spring.mapper.DozerMapper;
@@ -11,6 +12,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Slf4j
 @Service
@@ -24,25 +28,34 @@ public class PersonService {
 
     public PersonVO findById(Long id) {
         log.info("Find person");
-        return DozerMapper.parseObject(personRepository.findById(id)
-                .orElseThrow(() -> new NoSuchElementException("Person not found")), PersonVO.class);
+        Person person = personRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Person not found"));
+        PersonVO personVO = DozerMapper.parseObject(person, PersonVO.class);
+        personVO.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
+        return personVO;
     }
 
     public List<PersonVO> findAll() {
         log.info("Find all persons");
-        return DozerMapper.parseListObjects(personRepository.findAll(), PersonVO.class);
+        List<PersonVO> personVOS = DozerMapper.parseListObjects(personRepository.findAll(), PersonVO.class);
+        personVOS.forEach(personVO -> personVO.add(linkTo(methodOn(PersonController.class).findById(personVO.getPersonId())).withSelfRel()));
+        return personVOS;
     }
 
     public PersonVO save(PersonVO person) {
         log.info("Save person");
         Person personEntity = DozerMapper.parseObject(person, Person.class);
-        return DozerMapper.parseObject(personRepository.save(personEntity), PersonVO.class);
+        PersonVO personVO =  DozerMapper.parseObject(personRepository.save(personEntity), PersonVO.class);
+        personVO.add(linkTo(methodOn(PersonController.class).findById(personVO.getPersonId())).withSelfRel());
+        return personVO;
     }
 
     public PersonVOV2 saveV2(PersonVOV2 person) {
         log.info("Save person");
-        var entity = personMapper.convertVoTOEntity(person);
-        return personMapper.convertEntityToVo(personRepository.save(entity));
+        Person personEntity = personMapper.convertVoTOEntity(person);
+        PersonVOV2 personVOV2 =  personMapper.convertEntityToVo(personRepository.save(personEntity));
+        personVOV2.add(linkTo(methodOn(PersonController.class).findById(personVOV2.getPersonId())).withSelfRel());
+        return personVOV2;
     }
 
     public PersonVO update(Long id, PersonVO person) {
@@ -53,7 +66,9 @@ public class PersonService {
         personToUpdate.setGender(person.getGender());
         personToUpdate.setFirstName(person.getFirstName());
         personToUpdate.setLastName(person.getLastName());
-        return DozerMapper.parseObject(personRepository.save(DozerMapper.parseObject(person, Person.class)), PersonVO.class);
+        PersonVO personVO =  DozerMapper.parseObject(personRepository.save(DozerMapper.parseObject(person, Person.class)), PersonVO.class);
+        personVO.add(linkTo(methodOn(PersonController.class).findById(personVO.getPersonId())).withSelfRel());
+        return personVO;
     }
 
     public void delete(Long id) {
