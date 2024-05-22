d7ed328ddbc9a69c007c07d2ad2ad1370f6760cb
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-22 20:33:55 -0300

    Incluse PersonService interface in PersonController

--- src/main/java/br/com/rest/spring/controller/PersonController.java
+++ src/main/java/br/com/rest/spring/controller/PersonController.java
@@ -2,6 +2,7 @@ package br.com.rest.spring.controller;
 
 import br.com.rest.spring.data.vo.v1.PersonVO;
 import br.com.rest.spring.data.vo.v2.PersonVOV2;
+import br.com.rest.spring.service.PersonService;
 import br.com.rest.spring.service.impl.PersonServiceImpl;
 import br.com.rest.spring.util.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,50 +13,50 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/person/v1")
 public class PersonController {
-    private final PersonServiceImpl personServiceImpl;
+    private final PersonService personService;
 
     public PersonController(PersonServiceImpl personServiceImpl) {
-        this.personServiceImpl = personServiceImpl;
+        this.personService = personServiceImpl;
     }
 
 
     @GetMapping(produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML })
     public ResponseEntity<List<PersonVO>> findAll() {
-        return ResponseEntity.ok(personServiceImpl.findAll());
+        return ResponseEntity.ok(personService.findAll());
     }
 
     @GetMapping(value = "/{personId}",
             produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML })
     public ResponseEntity<PersonVO> findById(@PathVariable Long personId) {
-        return ResponseEntity.ok(personServiceImpl.findById(personId));
+        return ResponseEntity.ok(personService.findById(personId));
     }
 
     @PostMapping(
             consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML },
             produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML })
     public ResponseEntity<PersonVO> save(@RequestBody PersonVO person) {
-        return ResponseEntity.ok(personServiceImpl.save(person));
+        return ResponseEntity.ok(personService.save(person));
     }
 
     @PostMapping(value = "/v2",
             consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML },
             produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML })
     public ResponseEntity<PersonVOV2> saveV2(@RequestBody PersonVOV2 person) {
-        return ResponseEntity.ok(personServiceImpl.saveV2(person));
+        return ResponseEntity.ok(personService.saveV2(person));
     }
 
     @PutMapping(value = "/{personId}",
             consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML },
             produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML })
     public ResponseEntity<PersonVO> update(@PathVariable Long personId, @RequestBody PersonVO person) {
-        return ResponseEntity.ok(personServiceImpl.update(personId, person));
+        return ResponseEntity.ok(personService.update(personId, person));
     }
 
     @DeleteMapping(value = "/{personId}",
             consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML },
             produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML })
     public ResponseEntity<Void> delete(@PathVariable Long personId) {
-        personServiceImpl.delete(personId);
+        personService.delete(personId);
         return ResponseEntity.noContent().build();
     }
 
