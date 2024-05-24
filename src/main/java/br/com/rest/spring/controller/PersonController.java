8ff17b946f0024d36eeb7ea8caa39803c31f4652
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-24 08:16:35 -0300

    Added Documentation for findAll PersonController

--- src/main/java/br/com/rest/spring/controller/PersonController.java
+++ src/main/java/br/com/rest/spring/controller/PersonController.java
@@ -6,6 +6,12 @@ import br.com.rest.spring.service.PersonService;
 import br.com.rest.spring.service.impl.PersonServiceImpl;
 import br.com.rest.spring.util.MediaType;
 import com.sun.istack.NotNull;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/person/v1")
+@Tag(name = "People", description = "Endpoints for Management People")
 public class PersonController {
     private final PersonService personService;
 
@@ -22,6 +29,21 @@ public class PersonController {
 
 
     @GetMapping(produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML })
+    @Operation(summary = "Find all people recorded in the database", description = "Find all people recorded in the database"
+            , tags = { "People" }, responses = {
+            @ApiResponse(responseCode = "200", description = "People found", content = {
+                    @Content(mediaType = MediaType.APPLICATION_JSON,
+                            array = @ArraySchema(schema = @Schema(implementation = PersonVO.class))),
+                    @Content(mediaType = MediaType.APPLICATION_XML,
+                            array = @ArraySchema(schema = @Schema(implementation = PersonVO.class))),
+                    @Content(mediaType = MediaType.APPLICATION_YAML,
+                            array = @ArraySchema(schema = @Schema(implementation = PersonVO.class)))
+            }),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error" , content = @Content)
+    })
     public ResponseEntity<List<PersonVO>> findAll() {
         return ResponseEntity.ok(personService.findAll());
     }
