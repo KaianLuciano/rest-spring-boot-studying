2dc396712f7a93ce641548554d1f4ad13f295885
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-24 14:45:03 -0300

    Create V3__Create_Table_Books.sql

---
+++ src/main/resources/db/migration/V3__Create_Table_Books.sql
@@ -0,0 +1,7 @@
+CREATE TABLE tb_book (
+                       id SERIAL PRIMARY KEY,
+                       author TEXT,
+                       launch_date TIMESTAMP(6) NOT NULL,
+                       price NUMERIC(65,2) NOT NULL,
+                       title TEXT
+);
