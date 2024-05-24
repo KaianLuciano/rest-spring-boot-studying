b4bef19e6c61d0d0493aeffda64091c6c8cf08b6
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-24 14:44:43 -0300

    Create Book entity

---
+++ src/main/java/br/com/rest/spring/model/Book.java
@@ -0,0 +1,81 @@
+package br.com.rest.spring.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_book")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookId;
+    private String author;
+    private LocalDate launchDate;
+    private Double price;
+    private String title;
+
+    public Book() {}
+
+    public Book(Long bookId, String author, LocalDate launchDate, Double price, String title) {
+        this.bookId = bookId;
+        this.author = author;
+        this.launchDate = launchDate;
+        this.price = price;
+        this.title = title;
+    }
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDate getLaunchDate() {
+        return launchDate;
+    }
+
+    public void setLaunchDate(LocalDate launchDate) {
+        this.launchDate = launchDate;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book book)) return false;
+        return Objects.equals(getBookId(), book.getBookId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getBookId());
+    }
+}
