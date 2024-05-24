9acee67608d71b1aceba7c279870d56adc789d5a
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-23 23:00:02 -0300

    Added Exception and ResourceNotFoundException in GlobalExceptionHandler

--- src/main/java/br/com/rest/spring/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/br/com/rest/spring/exception/handler/GlobalExceptionHandler.java
@@ -1,7 +1,38 @@
 package br.com.rest.spring.exception.handler;
 
+import br.com.rest.spring.exception.handler.exception.ExceptionResponse;
+import br.com.rest.spring.exception.handler.exception.Exceptions;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ExceptionResponse> handleAllExceptions(
+            Exception ex, WebRequest request) {
+
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(Exceptions.ResourceNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(
+            Exception ex, WebRequest request) {
+
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
 }
