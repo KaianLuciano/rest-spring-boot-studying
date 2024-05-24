c0c0f9a9710fcb42548b2860ffd5cfca9f75f26a
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-23 22:40:31 -0300

    Added testCreateWithNullPerson in PersonServicesTest

--- src/test/java/br/com/rest/spring/unit/tests/mockito/services/PersonServicesTest.java
+++ src/test/java/br/com/rest/spring/unit/tests/mockito/services/PersonServicesTest.java
@@ -76,4 +76,17 @@ class PersonServicesTest {
         assertEquals("Female", result.getGender());
     }
 
+    @Test
+    void testCreateWithNullPerson() {
+        Exception exception = assertThrows(RequiredObjectIsNullException.class, () -> {
+            personServiceImpl.save(null);
+        });
+
+        String expectedMessage = "It is not allowed to persist a null object!";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+
 }
