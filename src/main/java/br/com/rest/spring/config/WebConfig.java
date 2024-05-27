d6b350d03469dd1fefc07cb87521a065c67f0e75
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-27 20:28:25 -0300

    Configured cors in application

--- src/main/java/br/com/rest/spring/config/WebConfig.java
+++ src/main/java/br/com/rest/spring/config/WebConfig.java
@@ -1,10 +1,12 @@
 package br.com.rest.spring.config;
 
 import br.com.rest.spring.serialization.converter.YamlJacksonToHttpMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -14,9 +16,21 @@ public class WebConfig implements WebMvcConfigurer {
 
     private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
 
+    @Value("${cors.originsPatterns:default}")
+    private String corsOriginPatterns = "";
+
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(new YamlJacksonToHttpMessageConverter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(",");
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins(allowedOrigins)
+                .allowCredentials(true);
     }
 
     @Override
