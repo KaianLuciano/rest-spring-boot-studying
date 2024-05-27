ff6f088309ad8e3529c769b46d26bdc04bcd5cfc
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-27 19:40:21 -0300

    Create AbstractIntegrationTest

--- src/test/java/br/com/rest/spring/integrationtests/testcontainers/AbstractIntegrationTest.java
+++ src/test/java/br/com/rest/spring/integrationtests/testcontainers/AbstractIntegrationTest.java
@@ -2,17 +2,44 @@ package br.com.rest.spring.integrationtests.testcontainers;
 
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
 import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.lifecycle.Startables;
 
-@ContextConfiguration(inheritInitializers = AbstractIntegrationTest.Initializer.class)
+import java.util.Map;
+import java.util.stream.Stream;
+
+@ContextConfiguration(initializers = AbstractIntegrationTest.Initializer.class)
 public class AbstractIntegrationTest {
 
     public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        static PostgreSQLContainer<?> postgresql = new PostgreSQLContainer<>("postgres:latest");
 
+        private static void startContainers() {
+            Startables.deepStart(Stream.of(postgresql)).join();
+        }
+
+        private static Map<String, String> createConnectionConfiguration() {
+            return Map.of(
+                    "spring.datasource.url", postgresql.getJdbcUrl(),
+                    "spring.datasource.username", postgresql.getUsername(),
+                    "spring.datasource.password", postgresql.getPassword(),
+                    "spring.datasource.driver-class-name", postgresql.getDriverClassName()
+            );
+        }
+
+        @SuppressWarnings({"unchecked", "rawtypes"})
         @Override
         public void initialize(ConfigurableApplicationContext applicationContext) {
-            System.setProperty("spring.datasource.url", "jdbc:tc:postgresql:13-alpine:///testdb");
-            System.setProperty("spring.datasource.username", "testuser");
-            System.setProperty("spring.datasource.password", "testpassword");
+            startContainers();
+            ConfigurableEnvironment environment = applicationContext.getEnvironment();
+            MapPropertySource testcontainers = new MapPropertySource(
+                    "testcontainers",
+                    (Map) createConnectionConfiguration()
+            );
+            environment.getPropertySources().addFirst(testcontainers);
         }
+    }
 }
