2b181d62afcacdc731712a721adf4bdb213745c2
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-28 21:42:05 -0300

    Create PersonVO of Tests

---
+++ src/test/java/br/com/rest/spring/integrationtests/vo/PersonVO.java
@@ -0,0 +1,67 @@
+package br.com.rest.spring.integrationtests.vo;
+
+import java.util.Objects;
+
+public class PersonVO {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String gender;
+
+    public PersonVO() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PersonVO personVO)) return false;
+        return Objects.equals(getId(), personVO.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
